def408b0e6fa1a2a509e14b56dec70b4f90fa54a
ankush789 <56834051+ankush789@users.noreply.github.com>
2021-08-03 15:35:47 +0530

    Completed NavBar component

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import './Navbar.css';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+  useEffect(() => {
+    showButton();
+  }, []);
+  window.addEventListener('resize', showButton);
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            TRVL <i class="fab fa-typo3"></i>
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            TRVL <i className="fab fa-typo3"></i>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
@@ -48,6 +64,7 @@ function Navbar() {
               </Link>
             </li>
           </ul>
+          {button && <Button buttonStyle="btn--outline">Sign Up</Button>}
         </div>
       </nav>
     </>
