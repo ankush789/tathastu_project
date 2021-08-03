e60b8b23b127302cd6bb6b3a136c89afa3e6fda2
ankush789 <56834051+ankush789@users.noreply.github.com>
2021-08-04 00:12:20 +0530

    SignUp form

--- src/component/Pages/SignUp.js
+++ src/component/Pages/SignUp.js
@@ -1,8 +1,52 @@
 import React from 'react';
+import { Button } from '../Button';
+
 import '../../App.css';
 
 const SignUp = () => {
-  return <h1 className="sign-up">SignUp</h1>;
+  return (
+    <div className="sign-up">
+      <div className="form-wrapper">
+        <form action="" method="post">
+          <div className="form_input">
+            <input
+              type="text"
+              name=""
+              id="name"
+              placeholder="Enter your name"
+            />
+          </div>
+          <div className="form_input">
+            <input
+              type="email"
+              name=""
+              id="email"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="form_input">
+            <input
+              type="password"
+              name=""
+              id="password"
+              placeholder="Enter a password"
+            />
+          </div>
+          <div className="form_input">
+            <input
+              type="password"
+              name=""
+              id="confirm_password"
+              placeholder="Renter password"
+            />
+          </div>
+          <div className="button">
+            <Button type="submit">Sign Up</Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
